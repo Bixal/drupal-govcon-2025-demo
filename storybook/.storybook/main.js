7ee36bd9f83221a5319a761a3e153a91b312c976
James Mejia <s.jamesmejia@gmail.com>
2025-08-09 17:59:02 -0500

    Embed USWDS storybook

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -11,5 +11,13 @@ const config = {
     name: "@storybook/html-vite",
     options: {},
   },
+  // ! Refer to the official USWDS documentation.
+  // ! Demo: embedding one Storybook in another.
+  refs: {
+    "uswds-clone": {
+      title: "USWDS Clone",
+      url: "https://6897cd2bcc83ef9c6701e275-lthpyxdfdd.chromatic.com",
+    },
+  },
 };
 export default config;
