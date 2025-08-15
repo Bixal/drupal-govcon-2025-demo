d7090f0c560ba220bdb06a0716e32e89f52d21aa
James Mejia <james.mejia@bixal.com>
2025-08-15 06:18:21 -0400

    Remove USWDS placeholder components

--- storybook/packages/components/identifier/Identifier.stories.js
+++
@@ -1,9 +0,0 @@
-import Identifier from "./identifier.html.twig";
-
-export default {
-  title: "Components/Identifier",
-  tags: ["autodocs"],
-  component: Identifier,
-};
-
-export const Default = {};
